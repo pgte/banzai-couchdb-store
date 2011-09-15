7c73019692379180b0b71da876423c70fafb18a6
Pedro Teixeira <pedro.teixeira@gmail.com>
2011-09-15 16:19:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var nano         = require('nano')
+  , EventEmitter = require('events').EventEmitter;
+
+module.exports = function(config) {
+  var creatingDB = false
+    , server, db;
+  
+  if (typeof(config) !== 'object') { throw new Error('Please define the config argument to this module as an object with .url and .db'); }
+  if (! config.url) { config.url = 'http://localhost:5984'; }
+  if (! config.db) { throw new Error('Please define config.db'); }
+  
+  server = nano(config.url);
+  db = server.use(config.db);
+  
+  function createDB(done) {
+    var replied = false;
+    
+    if (! creatingDB) {
+      creatingDB = new EventEmitter();
+      server.db.create(config.db, function(err) {
+
+        if (replied) { throw new Error('replied earlier!'); }
+        replied = true;
+
+        if (err) {
+          done(err);
+          creatingDB.emit('done', err);
+        } else {
+          done();
+          creatingDB.emit('done');
+        }
+        creatingDB = false
+      });
+    } else {
+      creatingDB.on('done', done);
+    }
+  }
+  
+  function load(docId, done, dontRepeat) {
+    var replied = false;
+    
+    db.get(docId, function(err, doc) {
+      if (replied) { throw new Error('replied earlier!'); }
+      replied = true;
+      if (err) {
+        if (err.message == 'no_db_file' && ! dontRepeat) {
+          return createDB(function(err) {
+            if (err) { return done(err); }
+            return load(docId, done, true);
+          })
+        }
+        return done(err);
+      }
+      if (doc._id) {
+        doc.id = doc._id;
+        delete doc._id;
+      }
+      done(null, doc);
+    });
+  }
+  
+  function save(doc, done, dontRepeat) {
+    var insertArgs = [doc]
+      , docId = doc.id || doc._id
+      , replied = false;
+      
+    if (docId) { insertArgs.push(docId); }
+    insertArgs.push(function(err, response) {
+
+      if (replied) { throw new Error('replied earlier!'); }
+      replied = true;
+      
+      if (err) {
+        if (err.message == 'no_db_file' && ! dontRepeat) {
+          return createDB(function(err) {
+            if (err) { return done(err); }
+            return save(doc, done, true);
+          })
+        }
+        return done(err);
+      }
+      
+      if (response.id) { doc.id = response.id; }
+      done(null, doc);
+    });
+    
+    db.insert.apply(db, insertArgs);
+  }
+  
+  return {
+      load: load
+    , save: save
+  };
+};
---
+++ test.js
@@ -0,0 +1,80 @@
+var store  = require('./')({db: 'test_doc_store'})
+  , assert = require('assert')
+  , EventEmitter = require('events').EventEmitter
+  , nano = require('nano')
+  , server = nano('http://localhost:5984')
+  , done = new EventEmitter();
+
+server.db.destroy('test_doc_store', function(err) {
+  done.emit('done');
+});
+
+module.exports.test_1 = function(beforeExit) {
+  var cb1 = false
+    , cb2 = false
+    , doc_id;
+  
+  done.once('done', function() {
+    store.save({a:1, b:2}, function(err, doc) {
+      assert.ok(! cb1);
+      cb1 = true;
+      assert.isNull(err);
+      assert.isNotNull(doc.id);
+      doc_id = doc.id;
+      delete doc.id;
+      assert.eql(doc, {a:1, b:2});
+
+      store.load(doc_id, function(err, doc) {
+        assert.ok(! cb2);
+        cb2 = true;
+        assert.isNull(err);
+        assert.isNotNull(doc.id);
+        assert.eql(doc.id, doc_id);
+        delete doc.id;
+        delete doc._rev;
+        assert.eql(doc, {a:1, b:2});
+      });
+
+    });
+  });
+  
+  beforeExit(function() {
+    assert.ok(cb1);
+    assert.ok(cb2);
+  });
+};
+
+module.exports.test_2 = function(beforeExit) {
+  var cb1 = false
+    , cb2 = false
+    , doc_id;
+
+  done.once('done', function() {
+    store.save({c:1, d:2}, function(err, doc) {
+      assert.ok(! cb1);
+      cb1 = true;
+      assert.isNull(err);
+      assert.isNotNull(doc.id);
+      doc_id = doc.id;
+      delete doc.id;
+      assert.eql(doc, {c:1, d:2});
+
+      store.load(doc_id, function(err, doc) {
+        assert.ok(! cb2);
+        cb2 = true;
+        assert.isNull(err);
+        assert.isNotNull(doc.id);
+        assert.eql(doc.id, doc_id);
+        delete doc.id;
+        delete doc._rev;
+        assert.eql(doc, {c:1, d:2});
+      });
+
+    });
+  });  
+  
+  beforeExit(function() {
+    assert.ok(cb1);
+    assert.ok(cb2);
+  });
+};
